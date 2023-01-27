b09ba90308384d1f929de68cc47919236f2fb180
Abhishek <abhisheknaikraver@gmail.com>
2023-01-27 18:55:20 +0530

    fist commit

---
+++ src/com/velocity/quiz/MainTest.java
@@ -0,0 +1,11 @@
+package com.velocity.quiz;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("new quiz stariting");
+
+	}
+
+}
---
+++ src/com/velocity/quiz/Student.java
@@ -0,0 +1,5 @@
+package com.velocity.quiz;
+
+public class Student {
+
+}
