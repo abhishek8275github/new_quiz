c9eafffca3ca464c388caa53b135cd84f3eae4fe
Abhishek <abhisheknaikraver@gmail.com>
2023-01-28 15:53:30 +0530

    connectinDb chance

--- src/com/velocity/quiz/ConnectionDb.java
+++ src/com/velocity/quiz/ConnectionDb.java
@@ -4,11 +4,14 @@ import java.sql.DriverManager;
 
 public class ConnectionDb {
 	Connection conection = null;
+	
+	public Connection dataBaseConnection() {
+	
 	String driver = "com.mysql.cj.jdbc.Driver";
 	String url = "jdbc:mysql://localhost:3306/quiz";
 	String uid ="root";
 	String pass="Abhishek 8275";
-	public Connection dataBaseConnection() {
+	
 		try {
 			Class.forName(driver);
 			conection = DriverManager.getConnection(url, uid, pass);
